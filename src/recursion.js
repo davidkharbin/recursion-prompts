591dce528ac8901779cfe0a9a04b221d209c217f
David Harbin <david_k_harbin@hotmail.com>
2021-04-10 21:54:02 -0600

    Completed isEven

--- src/recursion.js
+++ src/recursion.js
@@ -37,6 +37,19 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if ( n < 0 ) {
+    n = n + (n * -2);
+  }
+
+  if ( n === 1 ) {
+    return false;
+  }
+
+  if ( n === 0 ) {
+    return true;
+  }
+
+  return isEven(n - 2);
 };
 
 // 5. Sum all integers below a given integer.

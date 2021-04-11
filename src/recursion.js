347bb8845e2760691b221ce285f95fb167b48abe
David Harbin <david_k_harbin@hotmail.com>
2021-04-10 21:45:30 -0600

    Completed sum

--- src/recursion.js
+++ src/recursion.js
@@ -20,6 +20,14 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if ( array.length === 0 ) {
+    return 0;
+  }
+  if ( array.length === 1 ) {
+    return array[0];
+  }
+
+  return array[0] + sum(array.slice(1));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.

8ca4c1febf7f1956781e014f0fc9365801a63c9e
David Harbin <david_k_harbin@hotmail.com>
2021-04-10 21:43:53 -0600

    Completed factorial

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,14 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if ( n < 0 ) {
+    return null;
+  }
+  if ( n < 1 ) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // 2. Compute the sum of an array of integers.

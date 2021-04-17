4078afd43d0c3108099a3d476bbd13368add9a99
David Harbin <david_k_harbin@hotmail.com>
2021-04-16 19:10:30 -0600

    range() solved for positive exponents

--- src/recursion.js
+++ src/recursion.js
@@ -112,6 +112,15 @@ var range = function(start, end) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  if ( exp === 0 ) {
+    return 1;
+  }
+
+  if ( exp < 1 ) {
+    return base / exponent(base, exp + 1);
+  }
+
+  return base * exponent(base, exp - 1);
 };
 
 // 8. Determine if a number is a power of two.

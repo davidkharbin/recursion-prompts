fb3e904a31c74b35e098d7f3d665698800dd8eab
davidkharbin <david_k_harbin@hotmail.com>
2021-05-17 22:54:20 -0600

    working on createArray

--- src/recursion.js
+++ src/recursion.js
@@ -241,6 +241,16 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  var arr = [];
+  if ( str.length === 0 ) {
+    return ;
+  }
+
+  if ( str.length === 1 ) {
+    return [ str[0] ];
+  }
+
+  return arr.concat(createArray(str.slice(1)))
 };
 
 // 17. Reverse the order of an array

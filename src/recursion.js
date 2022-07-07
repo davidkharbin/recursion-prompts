7c604f7b20af1e8a38bc4ac48d2d5b5e336fb25d
davidkharbin <david_k_harbin@hotmail.com>
2022-07-06 22:09:27 -0600

    solved rMap

--- src/recursion.js
+++ src/recursion.js
@@ -298,6 +298,11 @@ let countOccurrence = function(array, value) {
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
 let rMap = function(array, callback) {
+  let result = []
+  if (!array.length) return result
+
+  result = result.concat(callback(array[0]), rMap(array.slice(1), callback))
+  return result
 };
 
 // 22. Write a function that counts the number of times a key occurs in an object.
